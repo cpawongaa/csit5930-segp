47d4c0a03b630a1b94e2259a72a0a016bca7842f
cpawongaa <cpawongaa@connect.ust.hk>
2023-04-18 11:51:21 +0800

    Completed build all indexes

---
+++ csit5930-segp-ws/SearchEngineWebApp/src/main/java/core/Base.java
@@ -0,0 +1,7 @@
+package core;
+
+import utils.Logger;
+
+public class Base extends Logger {
+
+}
---
+++ csit5930-segp-ws/SearchEngineWebApp/src/main/java/utils/StringUtil.java
@@ -0,0 +1,23 @@
+package utils;
+
+public class StringUtil {
+    
+    private static String special_characters = "\"`-=~!@#$%^&*()[]\\\\{}|;':\\\",./<>?\"";
+
+    public static void main(String[] args) {
+        String s = null;
+        
+        s = special_characters + "ABCabc123";
+        replaceSpecialCharacterToSpace(s);
+        
+        
+    }
+    
+    public static String replaceSpecialCharacterToSpace(String m_string) {
+        String result = null;
+        result = (m_string.replaceAll("[^0-9A-Za-z]", " ")).strip();
+//        System.out.println("replaceSpecialCharacterToSpace.result: [" + result + "]");
+        return result;
+    }
+
+}
